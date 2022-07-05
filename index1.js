717821844593e581e9efddba7dc4aa8a83cdfaf6
RahulMore <rahulmore1804@gmail.com>
2022-07-05 11:41:34 +0530

    3rd

--- index1.js
+++ index1.js
@@ -8,7 +8,7 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 550;
-canvas.height = 600;
+canvas.height = 500;
 
 const bulletController = new BulletController(canvas);
 const bulletController2 = new BulletController2(canvas);
